4d3cd6f15a6f8a7fb444168c09cdd0d724f1d7a1
czantoine <ac11f@free.fr>
2024-11-06 14:13:14 +1100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -63,7 +63,9 @@ app.get('/badge', async (req, res) => {
     }
 
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
         const page = await browser.newPage();
         await page.goto(`https://grafana.com/orgs/${user}/dashboards`);
 
